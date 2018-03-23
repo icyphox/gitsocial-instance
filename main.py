4c94f3f8a116c473c1cecb80ab71b0f3604e8ec1
icyphox <icyph0x@protonmail.com>
2018-03-23 20:35:41 +0530

    feat: create endpoints for add and edit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 IP_ADDR = ""
 
 @app.route("/api/get_instance")
-def serve_profile(methods=["POST"]):
+def serve_profile():
     user_hash = request.args.get('hash')
     if user_hash in os.listdir('repos/'):
         return send_from_directory(os.path.join('repos', user_hash), 'root.json')
@@ -16,8 +16,8 @@ def serve_profile(methods=["POST"]):
 
 
 @app.route("/api/search")
-def search_profiles(methods=['POST']):
-    user_name = reqquest.args.get("username")
+def search_profiles():
+    user_name = request.args.get("username")
     ret = []
     for dir_name in os.listdir("repos/"):
         try:
@@ -26,11 +26,23 @@ def search_profiles(methods=['POST']):
                 ret.append(dir_name)
     return jsonify(ret)
 
+@app.route('/api/add')
+def add_json():
+        
 
 
+@app.route('/api/edit')
+def edit_json():
+   nick = request.args.get('nick')
+   website = request.args.get('website')
+   following = request.args.get('following')
+   followers = request.args.get('followers')
+   
+   data = {user}
+
+   return "OK"
 
 
-    
 
 
 
